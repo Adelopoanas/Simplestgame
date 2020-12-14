b39b0648906c1b241a9569bfe1781f27cbdf23b5
Adelopoanas <aa.adelopoa@gmail.com>
2020-12-14 20:33:18 +0100

    Auto Commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var character = document.getElementById('character');
 var block = documemnt.getElementById('block');
 function jump() {
-    if (character.classList != "animate"){
+    if (character.classList != 'animate'){
         character.classList.add('animate');
     }
-    setTimeout(function () {
+    setTimeout(function() {
         character.classList.remove('animate');
     },500);
 }
-var checkDead = setInterval(function () {
+
+var checkDead = setInterval(function() {
     var character = 
     parseInt(window.getComputedStyle(character).getPropertyValue('top'));
 },10);
